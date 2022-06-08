8c74cfb43f27ce72dbd430930edb0bcbc6eb2a87
Eliyahu Tronto <a3938081@gmail.com>
2022-06-08 23:41:00 +0300

    it yountkj;lk

--- src/Main.java
+++ src/Main.java
@@ -7,4 +7,7 @@ public class Main {
     public static void M(int x){
         System.out.println("a ");
     }
+    public static void R(int m){
+        System.out.println("Hii what's up man ");
+    }
 }
